d9e604b409ee9a7af87987a0a4529704364e999c
okraus <okraus42+github@pm.me>
2024-03-20 13:46:07 +0100

    Buttons

--- src/ft_menu.c
+++ src/ft_menu.c
@@ -6,7 +6,7 @@
 /*   By: okraus <okraus@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 15:34:14 by okraus            #+#    #+#             */
-/*   Updated: 2024/03/19 16:40:02 by okraus           ###   ########.fr       */
+/*   Updated: 2024/03/20 13:40:51 by okraus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,41 @@ void	ft_initmenu(t_max *max)
 	max->menu.enter = 0;
 	max->menu.current_button = NEWGAME;
 	ft_load_texture("./textures/menu/labyrinth.png", &max->menu.background);
-	ft_load_texture("./textures/menu/button_newgame_1.png",  &max->menu.new_game_button_on);
-	ft_load_texture("./textures/menu/button_newgame_2.png",  &max->menu.new_game_button_off);
-	ft_load_texture("./textures/menu/button_quitgame_1.png",  &max->menu.quit_game_button_on);
-	ft_load_texture("./textures/menu/button_quitgame_2.png",  &max->menu.quit_game_button_off);
+	//ft_load_texture("./textures/menu/button_newgame_1_small.png", &max->menu.background);
+	ft_load_texture("./textures/menu/button_newgame_1_small.png",  &max->menu.new_game_button_on);
+	ft_load_texture("./textures/menu/button_newgame_2_small.png",  &max->menu.new_game_button_off);
+	ft_load_texture("./textures/menu/button_quitgame_1_small.png",  &max->menu.quit_game_button_on);
+	ft_load_texture("./textures/menu/button_quitgame_2_small.png",  &max->menu.quit_game_button_off);
+}
+
+void	ft_draw_button(t_max *max, mlx_texture_t *button, int x, int y)
+{
+	int				w;
+	int				h;
+	int				j;
+	int				i;
+	int				a;
+	unsigned int	c;
+
+	w = button->width;
+	h = button->height;
+	j = 0;
+	while (j < h)
+	{
+		i = 0;
+		while (i < w)
+		{
+			a = (j * w * 4) + (i * 4);
+			if (i < w && j < h)
+				c = (button->pixels[a]) << 24 | (button->pixels[a + 1]) << 16 | (button->pixels[a + 2]) << 8 | (button->pixels[a + 3]);
+			else
+				c = 0xFF00FFFF;
+			if ((c & 0xFF) == 0xFF)
+				mlx_put_pixel(max->menuscreen, i + x, j + y, c);
+			++i;
+		}
+		++j;
+	}
 }
 
 void	ft_draw_menu(t_max *max)
@@ -32,26 +63,31 @@ void	ft_draw_menu(t_max *max)
 	int				h;
 	int				y;
 	int				x;
+	int				a;
 	unsigned int	c;
 
 	w = max->menu.background->width;
 	h = max->menu.background->height;
 	y = 0;
-	while (y < HEIGHT)
+	//draw background
+	while (y < h)
 	{
 		x = 0;
-		while (x < WIDTH)
+		while (x < w)
 		{
-			if(x < w && y < h)
-				c = (unsigned int)(max->menu.background->pixels[(y * w) + (x * 4)]) << 24 | (unsigned int)(max->menu.background->pixels[(y * w) + (x * 4) + 1]) << 16 | (unsigned int)(max->menu.background->pixels[(y * w) + (x * 4) + 2]) << 8 | 0xFF;
+			a = (y * w * 4) + (x * 4);
+			if (x < w && y < h)
+				c = (max->menu.background->pixels[a]) << 24 | (max->menu.background->pixels[a + 1]) << 16 | (max->menu.background->pixels[a + 2]) << 8 | 0xFF;
 			else
 				c = 0xFF00FFFF;
-			//ft_printf("%i\n", y * WIDTH + x * 4);
 			mlx_put_pixel(max->menuscreen, x, y, c);
 			++x;
 		}
 		++y;
 	}
+	//draw button 1
+	ft_draw_button(max, max->menu.new_game_button_off, WIDTH / 2 - max->menu.new_game_button_off->width / 2, 200);
+	ft_draw_button(max, max->menu.quit_game_button_off, WIDTH / 2 - max->menu.quit_game_button_off->width / 2, 800);
 }
 
 void	ft_menu(t_max *max)
@@ -59,13 +95,11 @@ void	ft_menu(t_max *max)
 	ft_draw_menu(max);
 	if (max->menu.current_button == NEWGAME)
 	{
-		//remove highlight from all buttons
-		//highlight the new game button
+		ft_draw_button(max, max->menu.new_game_button_on, WIDTH / 2 - max->menu.new_game_button_on->width / 2, 200);
 	}
 	if (max->menu.current_button == QUITGAME)
 	{
-		//remove highlight from all buttons
-		//highlight the quit button
+		ft_draw_button(max, max->menu.quit_game_button_on, WIDTH / 2 - max->menu.quit_game_button_on->width / 2, 800);
 	}
 	if (max->menu.enter)
 	{
